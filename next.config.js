c3579dc1fa4daddd4ee8b9f9bffea70467be9df2
Midhun <midhunsanthakumar.97@gmail.com>
2025-04-17 18:54:26 +0530

    removed vercel tag

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  experimental: {
-    appDir: true, // enable App Router
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
