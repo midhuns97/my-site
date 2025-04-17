93a9eeb3cdfff3e50202f5d8f17eacbea5aa5240
Midhun <midhunsanthakumar.97@gmail.com>
2025-04-17 19:06:10 +0530

    metatag google 2

--- app/head.tsx
+++ app/head.tsx
@@ -2,16 +2,16 @@ export default function Head() {
     return (
       <>
         {/* Basic Meta Tags */}
+        <title>Midhun – Tech-Driven Growth Consultant</title>
+        <meta name="google-site-verification" content="9xvf2m08d3Mx88Bi0OX9bypznQydTGs4kcY6cKoN3xk" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
-          content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies."
-        />
+          content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies."/>
         <meta
           name="keywords"
-          content="Midhun, best consultant in India, software solutions expert, business growth strategies, digital transformation, strategic partnerships, business consultant India, tech entrepreneur"
-        />
+          content="Midhun, best consultant in India, software solutions expert, business growth strategies, digital transformation, strategic partnerships, business consultant India, tech entrepreneur"/>
         <meta name="author" content="Midhun S" />
         <meta
           name="google-site-verification"
