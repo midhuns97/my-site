7296f9dc6ffe4d7d7ba05e787564a4e57c522e43
Midhun <midhunsanthakumar.97@gmail.com>
2025-04-17 18:46:23 +0530

    Meta tags added 6 Opengraph back to 5

--- app/head.tsx
+++ app/head.tsx
@@ -10,39 +10,13 @@ export default function Head() {
   
         {/* Google Site Verification */}
         <meta name="google-site-verification" content="9xvf2m08d3Mx88Bi0OX9bypznQydTGs4kcY6cKoN3xk" />
-
-      {/* Facebook Meta Tags (Open Graph) */}
-      <meta property="og:url" content="https://www.midhuns.com" />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:title"
-        content="Midhun – Tech-Driven Growth Consultant"
-      />
-      <meta
-        property="og:description"
-        content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies."
-      />
-      <meta
-        property="og:image"
-        content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158"
-      />
-
-      {/* Twitter Meta Tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="midhuns.com" />
-      <meta property="twitter:url" content="https://www.midhuns.com" />
-      <meta
-        name="twitter:title"
-        content="Midhun – Tech-Driven Growth Consultant"
-      />
-      <meta
-        name="twitter:description"
-        content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies."
-      />
-      <meta
-        name="twitter:image"
-        content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158"
-      />
+  
+        {/* Open Graph */}
+        <meta property="og:title" content="Midhun – Tech-Driven Growth Consultant" />
+        <meta property="og:description" content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies." />
+        <meta property="og:image" content="https://www.midhuns.com/myimage.jpg" />
+        <meta property="og:url" content="https://www.midhuns.com" />
+        <meta property="og:type" content="website" />
 
         
       </>
