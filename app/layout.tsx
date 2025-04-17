c72cb1d2b5932d17ad398a6de1e1fe354dca8e1d
Midhun <midhunsanthakumar.97@gmail.com>
2025-04-17 19:31:12 +0530

    metatag google 6 image change

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         {/* ✅ Open Graph Meta */}
         <meta property="og:title" content="Midhun – Tech-Driven Growth Consultant" />
         <meta property="og:description" content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies." />
-        <meta property="og:image" content="https://www.midhuns.com/myimage.jpg" />
+        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158" />
         <meta property="og:url" content="https://www.midhuns.com" />
         <meta property="og:type" content="website" />
 
@@ -38,7 +38,7 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Midhun – Tech-Driven Growth Consultant" />
         <meta name="twitter:description" content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies." />
-        <meta name="twitter:image" content="https://www.midhuns.com/myimage.jpg" />
+        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158" />
 
         {/* ✅ Title Tag */}
         <title>Midhun – Tech-Driven Growth Consultant</title>
