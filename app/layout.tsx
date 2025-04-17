673b74c05a22bbfe5f363448b5eb8de3a153e03c
Midhun <midhunsanthakumar.97@gmail.com>
2025-04-17 17:49:02 +0530

    Meta tags added 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,30 +12,12 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+children,}: {children: React.ReactNode;}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <title>Midhun S | Software Consultant India | Business Strategist</title>
-        <meta name="title" content="Midhun S | Software Consultant India | Business Strategist" />
-        <meta name="description" content="Empowering Businesses with Custom Software Solutions & Strategic Partnerships" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.midhuns.com/" />
-        <meta property="og:title" content="Midhun S | Software Consultant India | Business Strategist" />
-        <meta property="og:description" content="Empowering Businesses with Custom Software Solutions & Strategic Partnerships" />
-        <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.midhuns.com/" />
-        <meta property="twitter:title" content="Midhun S | Software Consultant India | Business Strategist" />
-        <meta property="twitter:description" content="Empowering Businesses with Custom Software Solutions & Strategic Partnerships" />
-        <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
-
-      </head>
+      <Head>
+        <meta name="google-site-verification" content="9xvf2m08d3Mx88Bi0OX9bypznQydTGs4kcY6cKoN3xk" />
+      </Head>
       <body className={`${inter.className} dark`}>
         <ThemeProvider
           attribute="class"
