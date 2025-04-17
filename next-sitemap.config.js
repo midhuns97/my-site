0ab8422ab1aed18d5ff6c315b3ef7179e3239b0a
Midhun <midhunsanthakumar.97@gmail.com>
2025-04-17 15:47:00 +0530

    crawler

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://midhuns.com', // Don't include trailing slash
+    generateRobotsTxt: true,
+    generateIndexSitemap: false, // Optional if you want a single sitemap file
+  }
+  
