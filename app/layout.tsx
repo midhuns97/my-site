31f1456d08148c8bd733a098b1dd3cef067a1cb0
Midhun <midhunsanthakumar.97@gmail.com>
2025-04-21 12:33:56 +0530

    metatag google 7 content change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Midhun – Tech-Driven Growth Consultant',
+  title: 'Midhun – Best Software Consultant : Tech Solution Architect',
   description: 'Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies.',
 };
 
@@ -28,7 +28,7 @@ export default function RootLayout({
         <meta name="google-site-verification" content="9xvf2m08d3Mx88Bi0OX9bypznQydTGs4kcY6cKoN3xk" />
 
         {/* ✅ Open Graph Meta */}
-        <meta property="og:title" content="Midhun – Tech-Driven Growth Consultant" />
+        <meta property="og:title" content="Midhun – Best Software Consultant : Tech Solution Architect" />
         <meta property="og:description" content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies." />
         <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158" />
         <meta property="og:url" content="https://www.midhuns.com" />
@@ -36,12 +36,12 @@ export default function RootLayout({
 
         {/* ✅ Twitter Meta */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Midhun – Tech-Driven Growth Consultant" />
+        <meta name="twitter:title" content="Midhun – Best Software Consultant : Tech Solution Architect" />
         <meta name="twitter:description" content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies." />
         <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158" />
 
         {/* ✅ Title Tag */}
-        <title>Midhun – Tech-Driven Growth Consultant</title>
+        <title>Midhun – Best Software Consultant : Tech Solution Architect</title>
       </head>
       <body className={`${inter.className} dark`}>
         <ThemeProvider
