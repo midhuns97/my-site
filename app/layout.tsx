b2cf2c6722c4815662cd0056ab971038a2321c0b
Midhuns97 <midhunsanthakumar.97.com>
2025-11-15 16:27:05 +0530

    Favicon 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,13 +22,13 @@ export default function RootLayout({
         {/* ✅ Required Meta Tags */}
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Midhun, a top consultant in India, specializes in innovative software solutions and business growth strategies." />
+        <meta name="description" content="Midhun, a top consultant in UAE, specializes in innovative software solutions and brand growth strategies." />
 
         {/* ✅ Google Site Verification */}
         <meta name="google-site-verification" content="9xvf2m08d3Mx88Bi0OX9bypznQydTGs4kcY6cKoN3xk" />
 
         {/* ✅ Open Graph Meta */}
-        <meta property="og:title" content="Midhun – Software Consultant & Tech Solution Architect in Dubai" />
+        <meta property="og:title" content="Midhun – Software Sales Consultant & Brand Strategist in Dubai" />
         <meta property="og:description" content="Midhun, a top consultant in GCC, specializes in innovative software solutions and business growth strategies." />
         <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158" />
         <meta property="og:url" content="https://www.midhuns.com" />
@@ -41,8 +41,9 @@ export default function RootLayout({
         <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/6b288456-d3d5-4584-b2d1-6722c781d941.jpg?token=qVW3JyhpiP8zehViINtjO9RQqJu-2CQ-R_vA13umBus&height=675&width=1200&expires=33280895158" />
 
         {/* ✅ Title Tag */}
-        <title>Midhun – Software Consultant & Tech Solution Architect in Dubai</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Midhun – Software Sales Consultant & Brand Strategist in Dubai</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
       <body className={`${inter.className} dark`}>
         <ThemeProvider
